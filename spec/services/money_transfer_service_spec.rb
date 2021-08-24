e9cd88fd54c28b9b0a4efa1ffc6a9198d75a5b44
Santiago Diaz <santiago.d.diaz@gmail.com>
2021-08-24 03:16:50 -0300

    Add money transfer service spec

---
+++ spec/services/money_transfer_service_spec.rb
@@ -0,0 +1,17 @@
+describe MoneyTransferService do
+  let!(:user) { create(:user) }
+  let!(:payment_account) { create(:payment_account, user: user, balance: 100) }
+  let!(:external_payment_source) { create(:external_payment_source, user: user) }
+
+  subject do
+    MoneyTransferService.new(external_payment_source, user.payment_account).transfer(amount - user.payment_account_balance)
+  end
+
+  describe 'when the balance is not enough for the amount' do
+    let(:amount) { 300 }
+
+    it 'uses the money from the sender\'s external payment source' do
+      expect { subject }.to change { user.payment_account.reload.balance }.from(100).to(300)
+    end
+  end
+end
