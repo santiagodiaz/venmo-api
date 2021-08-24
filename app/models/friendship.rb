29d0e8941c7d975e5ff89d0ce87f296454915a65
Santiago Diaz <santiago.d.diaz@gmail.com>
2021-08-24 18:29:27 -0300

    Updates friendship validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -16,4 +16,19 @@
 class Friendship < ApplicationRecord
   belongs_to :friend_a, class_name: 'User'
   belongs_to :friend_b, class_name: 'User'
+
+  validate :friendship_uniqueness, on: %i[create update]
+
+  def self.exists_friendship(user, friend)
+    where(friend_a: user, friend_b: friend)
+      .or(Friendship.where(friend_a: friend, friend_b: user)).exists?
+  end
+
+  private
+
+  def friendship_uniqueness
+    return unless Friendship.exists_friendship(friend_a, friend_b)
+
+    errors.add(:friendship, I18n.t('api.errors.friendship_exists'))
+  end
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -22,4 +22,23 @@ RSpec.describe Friendship, type: :model do
     it { is_expected.to belong_to(:friend_a).class_name('User') }
     it { is_expected.to belong_to(:friend_b).class_name('User') }
   end
+
+  describe 'validations' do
+    context '.friendship_uniqueness' do
+      let!(:user_a) { create(:user) }
+      let!(:user_b) { create(:user) }
+      let!(:friendship) do
+        create(:friendship, friend_a: user_a, friend_b: user_b)
+      end
+
+      subject { build(:friendship, friend_a: user_b, friend_b: user_a) }
+
+      it 'returns an error message' do
+        subject.valid?
+        expect(subject.errors[:friendship]).to include(
+                                                 I18n.t('api.errors.friendship_exists')
+                                               )
+      end
+    end
+  end
 end
